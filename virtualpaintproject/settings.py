830aee66ad4a748d002b4af26331898f1dd95fc6
kamilvasilik <kamil.vasilik@gmail.com>
2020-10-19 07:43:48 +0200

    STATICFILES_DIRS moved from 'assets' to 'static'

--- virtualpaintproject/settings.py
+++ virtualpaintproject/settings.py
@@ -149,7 +149,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'assets')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'assets'),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 # STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
